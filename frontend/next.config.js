2dfb631b74fef98d245fd752a462bb0c5a610ba6
Gitterest <129048600+Gitterest@users.noreply.github.com>
2025-04-05 13:55:00 -0700

    🧨 Explicit postcss config forced via next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require('path');
 
-module.exports = nextConfig;
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              config: path.resolve(__dirname, 'postcss.config.js'),
+            },
+          },
+        },
+        'sass-loader',
+      ],
+    });
+    return config;
+  },
+};
