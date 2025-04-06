d6938f98453442da4f7b3f38c581aa7e1c7a29b7
Gitterest <129048600+Gitterest@users.noreply.github.com>
2025-04-05 20:32:26 -0700

    added more shit.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,16 +2,15 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./styles/**/*.{css,scss}",
+    "./styles/**/*.{css,scss}"
   ],
   safelist: [
     "p-0",
     "m-0",
-    "box-border"
+    "box-border",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
