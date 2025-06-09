3b548ae22fa053aeceb5d4f5e2ef142883251909
Gitterest <129048600+Gitterest@users.noreply.github.com>
2025-06-08 17:30:09 -0700

    added

--- frontend/pages/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE || 'https://sawprice-hunter-backend-production.up.railway.app/',
-  withCredentials: true,
-});
-
-export default API;
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://sawprice-hunter-backend-production.up.railway.app/',
-  withCredentials: true
+const API = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE || 'https://sawprice-hunter-backend-production.up.railway.app/',
+  withCredentials: true,
 });
 
-export default api;
+export default API;
